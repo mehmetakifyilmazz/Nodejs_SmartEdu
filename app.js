4275755f2571d8f10f09dde456eca2cf2b96a5cd
Mehmet Akif Yılmaz <124696212+mehmetakifyilmazz@users.noreply.github.com>
2023-09-14 19:19:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute')
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1/smartedu-db',{
+}).then(()=>{
+  console.log('db connected')
+});
+
+app.set('view engine', 'ejs');
+
+global.userIN = null;
+
+
+app.use(express.static('public'));
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+app.use(session({
+  secret: 'my_keybord_cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl:'mongodb://127.0.0.1/smartedu-db'})
+}))
+app.use(flash());
+app.use((req, res, next)=> {
+  res.locals.flashMessages = req.flash();
+  next();
+})
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+app.use('*', (req,res,next)=> {
+  userIN = req.session.userID;
+  next();
+})
+app.use(`/`, pageRoute);
+app.use(`/courses`, courseRoute);
+app.use(`/categories`, categoryRoute);
+app.use(`/users`, userRoute);
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
